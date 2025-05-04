42a5fd83e0ec3ea4803409749098087a76698941
satyammjha <satyammjha0@gmail.com>
2025-05-04 23:41:05 +0530

    bug:fix http blocking

--- server/server.ts
+++ server/server.ts
@@ -1,20 +1,27 @@
 import express from "express";
 import cors from "cors";
+import helmet from "helmet";
 import { Request, Response } from "express";
 import router from "./routes/getCommitRoute";
 import voicerouter from "./routes/voiceRoute";
 import messageRouter from "./routes/messageRoute"
 const app = express();
 
+app.use(helmet());
 
-app.get('/health', (req: Request, res: Response) => {
-    res.status(200).send("Hello World");
-});
+app.use(helmet.contentSecurityPolicy({
+    directives: {
+        upgradeInsecureRequests: [],
+    },
+}));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.json());
 
+app.get('/health', (req: Request, res: Response) => {
+    res.status(200).send("Hello World");
+});
 app.use('/', router)
 app.use('/', voicerouter);
 app.use('/', messageRouter);
