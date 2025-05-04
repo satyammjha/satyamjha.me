343e465845145ef96b9365cb6ea1262d2022e913
satyammjha <satyammjha0@gmail.com>
2025-05-04 22:20:39 +0530

    fix:port binding

--- server/server.ts
+++ server/server.ts
@@ -17,7 +17,6 @@ app.use(express.json());
 app.use('/', router)
 app.use('/', voicerouter);
 app.use('/', messageRouter);
-
-app.listen(5000, () => {
+app.listen(5000, '0.0.0.0', () => {
     console.log("Server is running on port 5000");
-})
+});
