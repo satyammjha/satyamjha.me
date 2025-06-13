47c964b400fc36093ca458c289f7a1c47545194b
Satyam Jha <satyammjha0@gmail.com>
2025-06-14 04:20:11 +0530

    Update server.ts

--- server/server.ts
+++ server/server.ts
@@ -1,30 +1,25 @@
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
-import { Request, Response } from "express";
 import router from "./routes/getCommitRoute";
 import voicerouter from "./routes/voiceRoute";
-import messageRouter from "./routes/messageRoute"
+import messageRouter from "./routes/messageRoute";
+
 const app = express();
 
 app.use(helmet());
-
-app.use(helmet.contentSecurityPolicy({
-    directives: {
-        upgradeInsecureRequests: [],
-    },
-}));
-
-app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/health', (req: Request, res: Response) => {
-    res.status(200).send("Hello World");
+app.get('/health', (req, res) => {
+    res.status(200).send("OK");
 });
-app.use('/', router)
+
+app.use('/', router);
 app.use('/', voicerouter);
 app.use('/', messageRouter);
+
 app.listen(5000, '0.0.0.0', () => {
-    console.log("Server is running on port 5000");
+    console.log("Server running on port 5000");
 });
